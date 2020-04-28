46f27625e0a7ea5fe121e3474e67c7558f308c33
Rene Nielsen <renenielsen107@hotmail.com>
2020-04-28 08:56:49 -0600

    First commit

---
+++ Library/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Library.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET /status - > 200 OK
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new StatusResponse
+            {
+                Message = "This is great stuff",
+                CheckedBy = "Joe Blow",
+                CheckedTimeStamp = DateTime.Now
+            };
+
+            return Ok(response);
+        }
+
+
+        public class StatusResponse
+        {
+            public string Message { get; set; }
+            public string CheckedBy { get; set; }
+            public DateTime CheckedTimeStamp { get; set; }
+        }
+    }
+}
